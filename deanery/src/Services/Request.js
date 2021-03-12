6fc69cdc175ac80c93966b0f5a1fce30adc56a63
Vasily <hod.vas@mail.ru>
2021-03-12 10:16:35 +0300

    git commit

--- deanery/src/Services/Request.js
+++ deanery/src/Services/Request.js
@@ -1,11 +1,11 @@
 const Request = {
-getStudents() {
-return fetch("http://localhost:8080/student/show/all").then(res => res.json());
-},
+  getStudents() {
+    return fetch("http://localhost:8080/student/show/all").then(res => res.json());
+  },
 
-getStudentsOnGroup(group) {
+  getStudentsOnGroup(group) {
     return fetch("http://localhost:8080/student/show/group/" + group).then(res => res.json());
-}
+  }
 };
 
 export default Request
